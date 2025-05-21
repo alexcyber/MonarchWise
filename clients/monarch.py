4489a3c2cd400fc46359192497f82e52136f4316
alexcyber <52591751+alexcyber@users.noreply.github.com>
2025-05-20 19:04:37 -0700

    Monarch tagging (#1)
    * Fixed MonarchMoney authentication and dependency issues
    
    * Fixed pagination issue where only the last 20 splitwise transactions were being evaluated
    
    * Integrates Splitwise and Monarch workflows with enhancements
    
    Refactors Monarch client to delegate login and transaction fetching logic to a helper module, improving code modularity and reusability. Introduces new functionalities for creating and managing tags, comparing Splitwise expenses with Monarch transactions, and synchronizing newly added expenses.
    
    Enhances Splitwise client to include group retrieval, filter out irrelevant expenses, and provide detailed expense data for synchronization. Updates the environment variable management to dynamically handle Splitwise's `updated_after` parameter for optimized syncing.
    
    Improves the main script to integrate these changes, ensuring better handling of account IDs and reducing redundant processing for subsequent runs.

--- clients/monarch.py
+++ clients/monarch.py
@@ -1,24 +1,120 @@
 from monarchmoney import MonarchMoney
 import json
 import hashlib
+import os
+from clients import monarch_helper as mhelper
+import re
 
 EXCLUDED_TRANSACTIONS_PATH = 'excluded.json'
 
 
 class MonarchClient(object):
     @classmethod
-    async def create(cls, email, password):
+    async def create(cls, email, password, uuid):
         self = cls()
         self.client = MonarchMoney()
         self.email = email
         self.password = password
-
-        await self.client.login(self.email, self.password)
+        self.uuid = uuid
+        self.client = await mhelper.login(self.client, {'username': email, 'password': password}, uuid)
         self.categories = (await self.client.get_transaction_categories())['categories']
         self.reimbursements_category_id = next(
-            (c for c in self.categories if c['name'] == 'Reimbursements'))['id']
+            (c for c in self.categories if c['name'] == 'Splitwise'))['id']
         return self
 
+    async def create_tag_dic(self):
+        original_tags = await self.client.get_transaction_tags()
+        tags = {}
+        for tag in original_tags['householdTransactionTags']:
+            tags[tag["name"]] = {"id": tag["id"], "color": tag["color"]}
+        return tags
+
+    async def new_find_matches(self, splitwise_expenses, mm_account_id):
+        '''
+        find_matches(self, splitwise_expenses):
+            #Remove old sw purchases
+            Compare SW expenses with excluded.json
+            Keep only new expenses
+            
+            Get MM tags for SW injested
+            
+            Get all transactions that have been created and tagged with SW
+                get all comments which include sw id
+            
+            for transaction in SW_expenses:
+                #Find out if transaction was already processed
+                Compare Price, date, tag, and note as hash?
+                if new:
+                    Add split transaction
+                    Add review
+                    Add note
+                    add tag
+                append to excluded
+        '''
+        mm_tags = await self.create_tag_dic()
+        mm_categories = await self.client.get_transaction_categories()
+        splitwise_category_id = next((cat['id'] for cat in mm_categories['categories'] if cat['name'] == 'Splitwise'), None)
+        
+        
+        mm_transactions = await mhelper.get_transactions(self.client, includeTags=mm_tags['From Splitwise']['id']) # Hardcoded value
+        # mm_transactions = await mhelper.get_transactions(self.client, limit=1000, synced_from_institution = False) # Hardcoded value
+        mm_sync_history = set()
+        for transaction in mm_transactions['allTransactions']['results']:
+            if transaction['amount'] < 0:
+                splitwise_id = re.search(r'Splitwise=(\d+)', transaction['notes'])
+                if splitwise_id:
+                    if splitwise_id in mm_sync_history:
+                        raise Exception(f"A duplicate SW_ID has been found in Monarch.  Something is wrong: {splitwise_id}")
+                    else:
+                        mm_sync_history.add(splitwise_id.group(1))
+        
+        if not mm_account_id:
+            message = ("Account ID to bill not specified.\n" +  
+                       "It's recommended to select the account you will be paying splitwise from.\n" +
+                       "For convienence, this is the account information.  Choose one and add it to .env:\n")
+            accounts_info = await self.client.get_accounts()
+            for account in accounts_info['accounts']:
+                message += f"\t{account['displayName']}: {account['id']}\n"
+            raise Exception(message)
+            
+        created_transactions = []
+        for expense in splitwise_expenses:
+            if str(expense['id']) in mm_sync_history:
+                continue
+                # Could add logic here to verify any changes in splitwise is reflected in monarch
+            
+            
+            merchant = f"SW - {expense['group_name']}"
+            notes = f"Splitwise {expense['group_name']}: {expense['description']}"
+            created_transaction = await self.client.create_transaction(
+                expense['date'],
+                mm_account_id,
+                expense['amount_owed'],
+                merchant,
+                splitwise_category_id,
+                f"{notes}\nSplitwise={expense['id']}", 
+                False
+            )
+            created_transaction_id = created_transaction['createTransaction']['transaction']['id']
+            created_transaction = await self.client.set_transaction_tags(created_transaction_id, mm_tags['From Splitwise']['id'] )
+            created_transaction = await self.client.update_transaction(created_transaction_id, needs_review=True)
+            
+
+            print(f"""Successfully created an expense with the following details: 
+            Expense Description: {notes}
+            Expense Cost: {expense['amount_owed']}
+            Group: {expense['group_name']}""")
+        
+        # created_transactions.append({'description': description,
+        #                         'cost': expense['amount_owed'],
+        #                         'groupId': {'name': expense['group_name']}})
+        # return created_transactions
+                
+            
+            
+            
+                
+    
     async def find_matches(self, splitwise_expenses):
         txns = []
         offset = 0
--- main.py
+++ main.py
@@ -3,24 +3,50 @@ import os
 from clients.splitwise import SplitwiseClient
 from clients.monarch import MonarchClient
 import asyncio
+from datetime import date, timedelta
 
-load_dotenv()
+env_path = os.path.join(os.path.dirname(__file__), '.env')
+load_dotenv(env_path)
 
 
 SPLITWISE_KEY = os.environ['SPLITWISE_KEY']
 SPLITWISE_SECRET = os.environ['SPLITWISE_SECRET']
 SPLITWISE_API_KEY = os.environ['SPLITWISE_API_KEY']
+SPLITWISE_UPDATED_AFTER = os.environ['SPLITWISE_UPDATED_AFTER']
 
 MONARCH_EMAIL = os.environ['MONARCH_EMAIL']
 MONARCH_PASSWORD = os.environ['MONARCH_PASSWORD']
+MONARCH_UUID = os.environ['MONARCH_UUID']
+try:
+    MONARCH_ACCOUNT_ID = os.environ['MONARCH_ACCOUNT_ID']
+except:
+    MONARCH_ACCOUNT_ID = None
 
+
+def update_env_variable(key, value):
+    lines = []
+    with open(env_path, "r") as file:
+        lines = file.readlines()
+
+    with open(env_path, "w") as file:
+        found = False
+        for line in lines:
+            if key in line:
+                file.write(f"{key}={value}\n")
+                found = True
+            else:
+                file.write(line)
+        if not found:
+            file.write(f"\n{key}={value}\n")
 
 async def main():
     s = SplitwiseClient(SPLITWISE_KEY, SPLITWISE_SECRET, SPLITWISE_API_KEY)
-    splitwise_expenses = s.get_expenses()
-
-    m = await MonarchClient.create(MONARCH_EMAIL, MONARCH_PASSWORD)
-    await m.find_matches(splitwise_expenses)
+    splitwise_expenses = s.get_expenses(SPLITWISE_UPDATED_AFTER)
+    # updates the dated_after variable in the .env file as an time enhancement for multiple runs
+    update_env_variable('SPLITWISE_UPDATED_AFTER', '"' + (date.today() - timedelta(days=5)).isoformat() + '"')
+    
+    m = await MonarchClient.create(MONARCH_EMAIL, MONARCH_PASSWORD, MONARCH_UUID)
+    expense_details = await m.new_find_matches(splitwise_expenses, MONARCH_ACCOUNT_ID)
 
 
 asyncio.run(main())
